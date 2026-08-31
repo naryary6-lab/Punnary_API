578b49c0866587ccc18f7a59077f03c83729f8e1
naryary6-lab <naryary6@gmail.com>
2026-09-01 03:48:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 
-const BOT_TOKEN = ''; /8679348511:AAGjUSodYvD16ig1bftVP7-xPeTWX993yiU/ 
-const ADMIN_CHAT_ID = '-https://t.me/+lziWwq7g4jxjZjQ1';//
+const BOT_TOKEN = '8679348511:AAGjUSodYvD16ig1bftVP7-xPeTWX993yiU/ 
+const ADMIN_CHAT_ID = '-https://t.me/+4RR5zLFLkvIxZTk9';//
+
